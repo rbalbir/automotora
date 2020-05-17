b41a0f0814feea252d6f78be776ff160b1615878
rodrigo <rbalbi2012@gmail.com>
2020-05-17 19:15:18 -0300

    Build

---
+++ build/precache-manifest.4183933c7688eec51d9a18cdf33f5c9b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b26b582e213c9380dd5d9e140d3abfcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e54f94dc7ef218d5ed8",
+    "url": "/static/css/2.ffe8cabf.chunk.css"
+  },
+  {
+    "revision": "e2d539416d29ed1335c6",
+    "url": "/static/css/main.f07a8a11.chunk.css"
+  },
+  {
+    "revision": "8e54f94dc7ef218d5ed8",
+    "url": "/static/js/2.aff8851e.chunk.js"
+  },
+  {
+    "revision": "e2d539416d29ed1335c6",
+    "url": "/static/js/main.67e77a89.chunk.js"
+  },
+  {
+    "revision": "3704b3e2b98e1a0d0bec",
+    "url": "/static/js/runtime-main.ea7eaeba.js"
+  },
+  {
+    "revision": "9d62cb9fcefe1cb94d2d90553cbef3f1",
+    "url": "/static/media/Proxima Nova Alt Bold.9d62cb9f.otf"
+  },
+  {
+    "revision": "e2602b2f2c615d20c880911364b32191",
+    "url": "/static/media/Proxima Nova Alt Light.e2602b2f.otf"
+  },
+  {
+    "revision": "56b54001dd42f5fb8eba8af36465b364",
+    "url": "/static/media/Proxima Nova Alt Thin.56b54001.otf"
+  },
+  {
+    "revision": "f401366193520cdf512c7ade98260e27",
+    "url": "/static/media/Proxima Nova Black.f4013661.otf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/static/media/Proxima Nova Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "b4f9eb8ce027016ab9b9860817451d07",
+    "url": "/static/media/Proxima Nova Extrabold.b4f9eb8c.otf"
+  },
+  {
+    "revision": "8f0bc01ce5e5becef482d277cb72b728",
+    "url": "/static/media/Proxima Nova Thin.8f0bc01c.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/static/media/ProximaNova-Regular.410504d4.otf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
